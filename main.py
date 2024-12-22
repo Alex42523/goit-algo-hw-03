061be545bf20bdd54215a6f399db5e8716ce82c5
Alex42523 <posdmy@gmail.com>
2024-12-22 11:06:43 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-from datetime import datetime
 import random
+from datetime import datetime
 def get_days_from_today(date):
     today = datetime.today()
-    date = datetime.strptime(date, "%Y-%m-%d")
+    try:
+        date = datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        print("Невірний тип дати")
+        return ""
     a = today - date
     return a.days
-print(get_days_from_today("2000-01-01"))
+print(get_days_from_today("2021-109"))
+print(get_days_from_today("2021-10-09"))
 def get_numbers_ticket(min, max, quantity):
-    if min >= 1 and min <= quantity <= max and max <= 1000:
-        unique_numbers = random.sample(range(min, max + 1), quantity)
-        return unique_numbers
-    else: return []
-print(get_numbers_ticket(1, 1000, 10))
-
+    if min >= 1 and max <= 1000 and min <= quantity <= max:
+        numbers = set()
+        while len(numbers) <= quantity:
+            number = random.randint(min, max)
+            numbers.add(number)
+        return sorted(list(numbers))
+    else:
+        return "Числа вибрані невірно"
+print(get_numbers_ticket(1, 100, 101))
+print(get_numbers_ticket(1, 100, 10))
