8daa232b5f18f10643bdb6c5a0c914ed83846cec
Alex42523 <posdmy@gmail.com>
2024-12-28 16:41:44 +0200

    Add files via upload

---
+++ mein.py
@@ -0,0 +1,22 @@
+import random
+from datetime import datetime
+def get_days_from_today(date):
+    today = datetime.today()
+    try:
+        date = datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        return "Невірний тип дати!"
+    a = today - date
+    return a.days
+print(get_days_from_today("2021-109"))
+print(get_days_from_today("2021-10-09"))
+def get_numbers_ticket(min, max, quantity):
+    if 1 <= min <= max <= 1000:
+        numbers = set()
+        while len(numbers) <= quantity:
+            number = random.randint(min, max)
+            numbers.add(number)
+        return sorted(numbers)
+    else:
+        return []
+print(get_numbers_ticket(10, 20, 5))
